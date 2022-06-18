c3e701d7565516b91ffff05f76ff2cc92be96855
Tiago Domingos Figueiredo <33582859+tiagodfigueiredo7@users.noreply.github.com>
2022-06-18 20:22:23 -0300

    Create vpc.tf

---
+++ vpc.tf
@@ -0,0 +1,9 @@
+# data para linkar uma Vpc existente 
+#data "aws_vpc" "vpc_aula" {
+#  id = "vpc-0ecac3f6c9205043b"
+   
+#}
+
+data "aws_subnet" "subnetaula" {
+  id = "subnet-0a583a8f1b6edcf27"
+}
